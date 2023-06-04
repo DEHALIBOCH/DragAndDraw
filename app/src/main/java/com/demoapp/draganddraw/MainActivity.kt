6375b64a3369b316f598a1df7ea991b87e5afad9
Marat <almabekov.marat@gmail.com>
2023-06-05 00:29:12 +0600

    Initial commit

---
+++ app/src/main/java/com/demoapp/draganddraw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demoapp.draganddraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
