b2a1e7d37502a0f7ea00b0f6ed8cf5481fc8c793
Marat <almabekov.marat@gmail.com>
2023-06-05 00:36:21 +0600

    Custom View

---
+++ app/src/main/java/com/demoapp/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,9 @@
+package com.demoapp.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+}
