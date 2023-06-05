393d7e99ee0b3697d1b258fcfb9b2f031b6b8299
Marat <almabekov.marat@gmail.com>
2023-06-05 23:41:11 +0600

    onTouchEvent function override to get x and y coordinates of touch.

--- app/src/main/java/com/demoapp/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/demoapp/draganddraw/BoxDrawingView.kt
@@ -1,9 +1,28 @@
 package com.demoapp.draganddraw
 
 import android.content.Context
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+
+private const val TAG = "BoxDrawingView"
 
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val current = PointF(event.x, event.y)
+        val action = when (event.action) {
+            MotionEvent.ACTION_MOVE -> "ACTION_MOVE"
+            MotionEvent.ACTION_CANCEL -> "ACTION_CANCEL"
+            MotionEvent.ACTION_DOWN -> "ACTION_DOWN"
+            MotionEvent.ACTION_UP -> "ACTION_UP"
+            else -> ""
+        }
+
+        Log.i(TAG, "$action at x = ${current.x}, y = ${current.y}")
+
+        return true
+    }
 }
